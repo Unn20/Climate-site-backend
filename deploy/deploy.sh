899f1d5c3885f145dca240796ea63af2b007e832
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2020-12-09 20:53:06 +0100

    FIX CI continued...

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,4 +11,4 @@ chmod +777 ./deploy/disable_host_checking.sh
 DEPLOY_SERVER=$DEPLOY_SERVER
 
 chmod +777 ./deploy/update_restart.sh
-ubuntu${DEPLOY_SERVER} 'bash' < ./deploy/update_restart.sh
+ubuntu@${DEPLOY_SERVER} 'bash' < ./deploy/update_restart.sh
