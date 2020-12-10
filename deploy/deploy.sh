f5a1769febbc168af7f7a0e4b106c57902bfd393
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2020-12-10 15:44:05 +0100

    Simplify deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,9 +5,6 @@ set -e
 eval $(ssh-agent -s)
 echo "$BACKEND_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
-chmod +777 ./deploy/disable_host_checking.sh
-./deploy/disable_host_checking.sh
-
 DEPLOY_SERVER=$DEPLOY_SERVER
 
 chmod +777 ./deploy/update_restart.sh
