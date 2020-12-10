db5aa55ff87b8ebbd17deb0f3152110bb9e72066
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2020-12-10 15:22:20 +0100

    Deploy fix

--- deploy/update_restart.sh
+++ deploy/update_restart.sh
@@ -2,30 +2,23 @@
 
 set -e
 
+pm2 stop server.js
+
+pm2 delete server.js
+
 rm -rf /home/ubuntu/backend/
 
-mkdir /home/ubuntu/backend
-cd /home/ubuntu/backend
+cd /home/ubuntu/
 
 git clone git@gitlab.com:inzynierka3/backend.git
 
 PATH=/home/ubuntu/node/bin:$PATH
 
-# stop the previous pm2
-pm2 kill
-npm remove pm2 -g
-
-#pm2 needs to be installed globally as we would be deleting the repo folder.
-# this needs to be done only once as a setup script.
-npm install pm2 -g
-# starting pm2 daemon
-pm2 status
-
 cd /home/ubuntu/backend
 
-#install npm packages
-echo "Running npm install"
 npm install
+
+pm2 start server.js
 
 #Restart the node server
 npm start
