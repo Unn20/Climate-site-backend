0bdb57776132975ec1c9523362c0800f236559f3
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2020-12-15 19:05:31 +0100

    Fixed issues from comments

--- deploy/update_restart.sh
+++ deploy/update_restart.sh
@@ -2,18 +2,16 @@
 
 set -e
 
-pm2 delete backend 2 > /dev/null || true
 
-rm -rf /home/ubuntu/backend/
+pm2 stop backend 2 > /dev/null || true
 
-cd /home/ubuntu/
+cd /home/ubuntu/backend/
 
-git clone git@gitlab.com:inzynierka3/backend.git
+git checkout master
+git pull git@gitlab.com:inzynierka3/backend.git master
 
 PATH=/home/ubuntu/node/bin:$PATH
 
-cd /home/ubuntu/backend
-
 npm install
 
-pm2 start server.js --name backend --watch
+(pm2 restart backend) || ( (pm2 delete backend 2 > /dev/null) && (pm2 start server.js --name backend --watch) )
