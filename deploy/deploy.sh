2ce66023eb66916e589dd6968d3f3a0049863293
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2020-12-15 19:08:43 +0100

    Missing whitespace

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,5 +6,5 @@ echo "$BACKEND_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
 DEPLOY_SERVER=$DEPLOY_SERVER
 
-chmod +x./deploy/update_restart.sh
+chmod +x ./deploy/update_restart.sh
 ssh ubuntu@${DEPLOY_SERVER} 'bash' < ./deploy/update_restart.sh
