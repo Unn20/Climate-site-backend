f536a94688f941ed8b25db563bf36d6f387cae0b
Unn20 <maciej.leszczyk@student.put.poznan.pl>
2020-12-28 14:57:18 +0100

    Added logging for apis

--- api/counters.js
+++ api/counters.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dataBaseConnector = require("../database/data-base-connector");
 const appCounters = express();
+const logger = require('../logger');
 
 // static data for day 16.12.2020 22:30 (today)
 // 1. tons of co2 into the atmosphere https://www.theworldcounts.com/challenges/climate-change/global-warming/global-co2-emissions/story
@@ -10,13 +11,13 @@ const appCounters = express();
 // 5. tons of resorces extracted from earth https://www.theworldcounts.com/challenges/planet-earth/state-of-the-planet/resources-extracted-from-earth/story
 // 6. tons of plastic dumped in oceans https://www.theworldcounts.com/challenges/planet-earth/oceans/plastic-in-the-ocean/story
 const countersDataBackup = [{
-    id:"2020-12-16T22:30:44.000Z",
-    carbon_dioxide:11086000,
-    melted_id:1925080,
-    tera_joules_used:198150,
-    waste_dumped:5464000,
-    resources_extracted:228391000,
-    plastic_in_ocean:32765
+    id: "2020-12-16T22:30:44.000Z",
+    carbon_dioxide: 11086000,
+    melted_id: 1925080,
+    tera_joules_used: 198150,
+    waste_dumped: 5464000,
+    resources_extracted: 228391000,
+    plastic_in_ocean: 32765
 }];
 
 async function getCountersData() {
@@ -26,6 +27,7 @@ async function getCountersData() {
 
 appCounters.route('/').get(async (req, res) => {
     let countersData = await getCountersData();
+    logger.info(`${req.baseUrl} has been fetched from ${req.hostname}:${req.ip}`)
     res.send(countersData);
 })
 
