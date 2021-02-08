faca5fb4960051c5932bdf13f0f578d6192eea2e
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2021-02-08 23:01:08 +0100

    Forgot to commit before pushing

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-echo "$BACKEND_KEY" | tr -d '\r' | ssh-add - > /dev/null
+# echo "$BACKEND_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
-DEPLOY_SERVER=$DEPLOY_SERVER
+# DEPLOY_SERVER=$DEPLOY_SERVER
 
 chmod +x ./deploy/update_restart.sh
 ssh ubuntu@${DEPLOY_SERVER} 'bash' < ./deploy/update_restart.sh
