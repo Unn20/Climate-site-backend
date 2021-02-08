3de8815dcef24bbadd4efdeb485777900508fb01
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2021-02-08 23:08:24 +0100

    Removed comments

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,9 +2,5 @@
 
 set -e
 
-# echo "$BACKEND_KEY" | tr -d '\r' | ssh-add - > /dev/null
-
-# DEPLOY_SERVER=$DEPLOY_SERVER
-
 chmod +x ./deploy/update_restart.sh
 ssh ubuntu@${DEPLOY_SERVER} 'bash' < ./deploy/update_restart.sh
